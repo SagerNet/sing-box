223f68b7c3d14ddc4e914af00445990b342d9a68
世界 <i@sekai.icu>
2022-07-24 21:29:39 +0800

    Add default tcp keep alive settings

--- constant/timeout.go
+++ constant/timeout.go
@@ -3,8 +3,9 @@ package constant
 import "time"
 
 const (
-	DefaultTCPTimeout      = 5 * time.Second
-	ReadPayloadTimeout     = 300 * time.Millisecond
-	URLTestTimeout         = DefaultTCPTimeout
-	DefaultURLTestInterval = 1 * time.Minute
+	DefaultTCPTimeout         = 5 * time.Second
+	DefaultTCPKeepAlivePeriod = 20 * time.Second
+	ReadPayloadTimeout        = 300 * time.Millisecond
+	URLTestTimeout            = DefaultTCPTimeout
+	DefaultURLTestInterval    = 1 * time.Minute
 )
