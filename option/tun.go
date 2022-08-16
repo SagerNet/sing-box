c16596939936485928e2160c8c9ae41cc366ccde
世界 <i@sekai.icu>
2022-08-16 12:16:59 +0800

    Fix include_android_user option

--- option/tun.go
+++ option/tun.go
@@ -10,7 +10,7 @@ type TunInboundOptions struct {
 	IncludeUIDRange        Listable[string] `json:"include_uid_range,omitempty"`
 	ExcludeUID             Listable[uint32] `json:"exclude_uid,omitempty"`
 	ExcludeUIDRange        Listable[string] `json:"exclude_uid_range,omitempty"`
-	IncludeAndroidUser     Listable[int]    `json:"android_user,omitempty"`
+	IncludeAndroidUser     Listable[int]    `json:"include_android_user,omitempty"`
 	IncludePackage         Listable[string] `json:"include_package,omitempty"`
 	ExcludePackage         Listable[string] `json:"exclude_package,omitempty"`
 	EndpointIndependentNat bool             `json:"endpoint_independent_nat,omitempty"`
