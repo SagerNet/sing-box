bfcf1017f0c718a6d633a4d9ae1a074cfd2fcaa5
世界 <i@sekai.icu>
2022-08-26 21:53:08 +0800

    Add strict_route option

--- option/tun.go
+++ option/tun.go
@@ -6,6 +6,7 @@ type TunInboundOptions struct {
 	Inet4Address           *ListenPrefix    `json:"inet4_address,omitempty"`
 	Inet6Address           *ListenPrefix    `json:"inet6_address,omitempty"`
 	AutoRoute              bool             `json:"auto_route,omitempty"`
+	StrictRoute            bool             `json:"strict_route,omitempty"`
 	IncludeUID             Listable[uint32] `json:"include_uid,omitempty"`
 	IncludeUIDRange        Listable[string] `json:"include_uid_range,omitempty"`
 	ExcludeUID             Listable[uint32] `json:"exclude_uid,omitempty"`
