df023f7bd6d1aabc5c6dc418f2ee2e4e84cb2315
isnowly <raycke@pm.me>
2022-12-12 16:57:16 +0800

    Fix windows proxy

--- common/settings/proxy_windows.go
+++ common/settings/proxy_windows.go
@@ -7,7 +7,7 @@ import (
 )
 
 func SetSystemProxy(router adapter.Router, port uint16, isMixed bool) (func() error, error) {
-	err := wininet.SetSystemProxy(F.ToString("http://127.0.0.1:", port), "<local>")
+	err := wininet.SetSystemProxy(F.ToString("127.0.0.1:", port), "localhost;127.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;192.168.*;<local>")
 	if err != nil {
 		return nil, err
 	}
