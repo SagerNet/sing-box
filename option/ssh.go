84fdd08b0e617b09ee219028e0594652171a2a87
lyc8503 <36782264+lyc8503@users.noreply.github.com>
2023-02-08 16:20:48 +0800

    Add SSH outbound host key validation

--- option/ssh.go
+++ option/ssh.go
@@ -8,6 +8,7 @@ type SSHOutboundOptions struct {
 	PrivateKey           string           `json:"private_key,omitempty"`
 	PrivateKeyPath       string           `json:"private_key_path,omitempty"`
 	PrivateKeyPassphrase string           `json:"private_key_passphrase,omitempty"`
+	HostKey              Listable[string] `json:"host_key,omitempty"`
 	HostKeyAlgorithms    Listable[string] `json:"host_key_algorithms,omitempty"`
 	ClientVersion        string           `json:"client_version,omitempty"`
 }
