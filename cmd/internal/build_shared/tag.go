027d164dbd67d879745a1490a3fb9e572acb5bea
世界 <i@sekai.icu>
2023-03-13 11:31:36 +0800

    Print command to shell error

--- cmd/internal/build_shared/tag.go
+++ cmd/internal/build_shared/tag.go
@@ -1,18 +1,16 @@
 package build_shared
 
-import (
-	"github.com/sagernet/sing/common"
-)
+import "github.com/sagernet/sing/common/shell"
 
 func ReadTag() (string, error) {
-	currentTag, err := common.Exec("git", "describe", "--tags").ReadOutput()
+	currentTag, err := shell.Exec("git", "describe", "--tags").ReadOutput()
 	if err != nil {
 		return currentTag, err
 	}
-	currentTagRev, _ := common.Exec("git", "describe", "--tags", "--abbrev=0").ReadOutput()
+	currentTagRev, _ := shell.Exec("git", "describe", "--tags", "--abbrev=0").ReadOutput()
 	if currentTagRev == currentTag {
 		return currentTag[1:], nil
 	}
-	shortCommit, _ := common.Exec("git", "rev-parse", "--short", "HEAD").ReadOutput()
+	shortCommit, _ := shell.Exec("git", "rev-parse", "--short", "HEAD").ReadOutput()
 	return currentTagRev[1:] + "-" + shortCommit, nil
 }
