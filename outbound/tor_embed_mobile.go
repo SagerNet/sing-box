f674b4fbd5f17fd8f6b3597caa7e137167acb022
世界 <i@sekai.icu>
2023-03-15 20:59:45 +0800

    Fix build embed tor for mobile

--- outbound/tor_embed_mobile.go
+++ outbound/tor_embed_mobile.go
@@ -1,10 +1,10 @@
-//go:build with_embedded_tor && !(android || ios)
+//go:build with_embedded_tor && (android || ios)
 
 package outbound
 
 import (
-	"berty.tech/go-libtor"
 	"github.com/cretz/bine/tor"
+	"github.com/ooni/go-libtor"
 )
 
 func newConfig() tor.StartConf {
