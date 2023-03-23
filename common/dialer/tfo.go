119a1dccb19d53608b0eda0562dc8f0ec4f9962f
世界 <i@sekai.icu>
2023-03-23 17:14:38 +0800

    Fix slow open

--- common/dialer/tfo.go
+++ common/dialer/tfo.go
@@ -119,6 +119,10 @@ func (c *slowOpenConn) LazyHeadroom() bool {
 	return c.conn == nil
 }
 
+func (c *slowOpenConn) NeedHandshake() bool {
+	return c.conn == nil
+}
+
 func (c *slowOpenConn) ReadFrom(r io.Reader) (n int64, err error) {
 	if c.conn != nil {
 		return bufio.Copy(c.conn, r)
