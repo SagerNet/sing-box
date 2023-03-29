879c38351b026079fbbf0e7c5d92ac494568869a
世界 <i@sekai.icu>
2023-03-29 10:30:31 +0800

    Accept "any" outbound in dns rule

--- route/rule_outbound.go
+++ route/rule_outbound.go
@@ -12,17 +12,25 @@ var _ RuleItem = (*OutboundItem)(nil)
 type OutboundItem struct {
 	outbounds   []string
 	outboundMap map[string]bool
+	matchAny    bool
 }
 
 func NewOutboundRule(outbounds []string) *OutboundItem {
-	rule := &OutboundItem{outbounds, make(map[string]bool)}
+	rule := &OutboundItem{outbounds: outbounds, outboundMap: make(map[string]bool)}
 	for _, outbound := range outbounds {
-		rule.outboundMap[outbound] = true
+		if outbound == "any" {
+			rule.matchAny = true
+		} else {
+			rule.outboundMap[outbound] = true
+		}
 	}
 	return rule
 }
 
 func (r *OutboundItem) Match(metadata *adapter.InboundContext) bool {
+	if r.matchAny && metadata.Outbound != "" {
+		return true
+	}
 	return r.outboundMap[metadata.Outbound]
 }
 
