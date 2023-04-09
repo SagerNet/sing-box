a3728505b9aa4dd5abddf2a3c2830af6f2c2e80f
世界 <i@sekai.icu>
2023-04-09 12:39:26 +0800

    clash api: download clash-dashboard if external-ui directory is empty

---
+++ common/badversion/version_json.go
@@ -0,0 +1,17 @@
+package badversion
+
+import "github.com/sagernet/sing-box/common/json"
+
+func (v Version) MarshalJSON() ([]byte, error) {
+	return json.Marshal(v.String())
+}
+
+func (v *Version) UnmarshalJSON(data []byte) error {
+	var version string
+	err := json.Unmarshal(data, &version)
+	if err != nil {
+		return err
+	}
+	*v = Parse(version)
+	return nil
+}
