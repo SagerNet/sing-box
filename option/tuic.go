f46732bc0e43a07ebf7baa9e2e1be19ab670e995
世界 <i@sekai.icu>
2023-08-20 11:28:30 +0800

    Add udp over stream support for TUIC

--- option/tuic.go
+++ option/tuic.go
@@ -23,6 +23,7 @@ type TUICOutboundOptions struct {
 	Password          string              `json:"password,omitempty"`
 	CongestionControl string              `json:"congestion_control,omitempty"`
 	UDPRelayMode      string              `json:"udp_relay_mode,omitempty"`
+	UDPOverStream     bool                `json:"udp_over_stream,omitempty"`
 	ZeroRTTHandshake  bool                `json:"zero_rtt_handshake,omitempty"`
 	Heartbeat         Duration            `json:"heartbeat,omitempty"`
 	Network           NetworkList         `json:"network,omitempty"`
