53475c739054edefb8082d8242d8a6e1602e6eb9
世界 <i@sekai.icu>
2023-08-31 20:07:32 +0800

    Add hysteria2 protocol

--- inbound/hysteria_stub.go
+++ inbound/hysteria_stub.go
@@ -14,3 +14,7 @@ import (
 func NewHysteria(ctx context.Context, router adapter.Router, logger log.ContextLogger, tag string, options option.HysteriaInboundOptions) (adapter.Inbound, error) {
 	return nil, C.ErrQUICNotIncluded
 }
+
+func NewHysteria2(ctx context.Context, router adapter.Router, logger log.ContextLogger, tag string, options option.Hysteria2InboundOptions) (adapter.Inbound, error) {
+	return nil, C.ErrQUICNotIncluded
+}
