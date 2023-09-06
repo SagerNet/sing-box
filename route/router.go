4095dcbf05ba017562b6d37aa2b347d1aa3e7c84
世界 <i@sekai.icu>
2023-09-06 19:33:17 +0800

    Reject invalid connection

--- route/router.go
+++ route/router.go
@@ -700,6 +700,9 @@ func (r *Router) RouteConnection(ctx context.Context, conn net.Conn, metadata ad
 	if !common.Contains(detour.Network(), N.NetworkTCP) {
 		return E.New("missing supported outbound, closing connection")
 	}
+	if !metadata.Destination.IsValid() && len(metadata.DestinationAddresses) == 0 {
+		return E.New("invalid destination address")
+	}
 	if r.clashServer != nil {
 		trackerConn, tracker := r.clashServer.RoutedConnection(ctx, conn, metadata, matchedRule)
 		defer tracker.Leave()
@@ -811,6 +814,9 @@ func (r *Router) RoutePacketConnection(ctx context.Context, conn N.PacketConn, m
 	}
 	if !common.Contains(detour.Network(), N.NetworkUDP) {
 		return E.New("missing supported outbound, closing packet connection")
+	}
+	if !metadata.Destination.IsValid() && len(metadata.DestinationAddresses) == 0 {
+		return E.New("invalid destination address")
 	}
 	if r.clashServer != nil {
 		trackerConn, tracker := r.clashServer.RoutedPacketConnection(ctx, conn, metadata, matchedRule)
