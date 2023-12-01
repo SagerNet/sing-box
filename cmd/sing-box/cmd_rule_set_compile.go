f1b92500fafd1f2fe011a370e132bd694ecaee73
世界 <i@sekai.icu>
2023-12-01 20:15:11 +0800

    Migrate contentjson and badjson to library & Add omitempty in format

--- cmd/sing-box/cmd_rule_set_compile.go
+++ cmd/sing-box/cmd_rule_set_compile.go
@@ -5,10 +5,10 @@ import (
 	"os"
 	"strings"
 
-	"github.com/sagernet/sing-box/common/json"
 	"github.com/sagernet/sing-box/common/srs"
 	"github.com/sagernet/sing-box/log"
 	"github.com/sagernet/sing-box/option"
+	"github.com/sagernet/sing/common/json"
 
 	"github.com/spf13/cobra"
 )
