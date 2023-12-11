6ddcd3954d65779861ef589ae98202be1c2a85eb
世界 <i@sekai.icu>
2023-12-11 18:36:06 +0800

    Refactor inbound/outbound options struct

--- option/vmess.go
+++ option/vmess.go
@@ -2,8 +2,8 @@ package option
 
 type VMessInboundOptions struct {
 	ListenOptions
-	Users     []VMessUser              `json:"users,omitempty"`
-	TLS       *InboundTLSOptions       `json:"tls,omitempty"`
+	Users []VMessUser `json:"users,omitempty"`
+	InboundTLSOptionsContainer
 	Multiplex *InboundMultiplexOptions `json:"multiplex,omitempty"`
 	Transport *V2RayTransportOptions   `json:"transport,omitempty"`
 }
@@ -17,14 +17,14 @@ type VMessUser struct {
 type VMessOutboundOptions struct {
 	DialerOptions
 	ServerOptions
-	UUID                string                    `json:"uuid"`
-	Security            string                    `json:"security"`
-	AlterId             int                       `json:"alter_id,omitempty"`
-	GlobalPadding       bool                      `json:"global_padding,omitempty"`
-	AuthenticatedLength bool                      `json:"authenticated_length,omitempty"`
-	Network             NetworkList               `json:"network,omitempty"`
-	TLS                 *OutboundTLSOptions       `json:"tls,omitempty"`
-	PacketEncoding      string                    `json:"packet_encoding,omitempty"`
-	Multiplex           *OutboundMultiplexOptions `json:"multiplex,omitempty"`
-	Transport           *V2RayTransportOptions    `json:"transport,omitempty"`
+	UUID                string      `json:"uuid"`
+	Security            string      `json:"security"`
+	AlterId             int         `json:"alter_id,omitempty"`
+	GlobalPadding       bool        `json:"global_padding,omitempty"`
+	AuthenticatedLength bool        `json:"authenticated_length,omitempty"`
+	Network             NetworkList `json:"network,omitempty"`
+	OutboundTLSOptionsContainer
+	PacketEncoding string                    `json:"packet_encoding,omitempty"`
+	Multiplex      *OutboundMultiplexOptions `json:"multiplex,omitempty"`
+	Transport      *V2RayTransportOptions    `json:"transport,omitempty"`
 }
