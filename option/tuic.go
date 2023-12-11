27e68df931acd721622808e2074acd0e953f18f6
世界 <i@sekai.icu>
2023-12-11 18:36:06 +0800

    Refactor inbound/outbound options struct

--- option/tuic.go
+++ option/tuic.go
@@ -2,12 +2,12 @@ package option
 
 type TUICInboundOptions struct {
 	ListenOptions
-	Users             []TUICUser         `json:"users,omitempty"`
-	CongestionControl string             `json:"congestion_control,omitempty"`
-	AuthTimeout       Duration           `json:"auth_timeout,omitempty"`
-	ZeroRTTHandshake  bool               `json:"zero_rtt_handshake,omitempty"`
-	Heartbeat         Duration           `json:"heartbeat,omitempty"`
-	TLS               *InboundTLSOptions `json:"tls,omitempty"`
+	Users             []TUICUser `json:"users,omitempty"`
+	CongestionControl string     `json:"congestion_control,omitempty"`
+	AuthTimeout       Duration   `json:"auth_timeout,omitempty"`
+	ZeroRTTHandshake  bool       `json:"zero_rtt_handshake,omitempty"`
+	Heartbeat         Duration   `json:"heartbeat,omitempty"`
+	InboundTLSOptionsContainer
 }
 
 type TUICUser struct {
@@ -19,13 +19,13 @@ type TUICUser struct {
 type TUICOutboundOptions struct {
 	DialerOptions
 	ServerOptions
-	UUID              string              `json:"uuid,omitempty"`
-	Password          string              `json:"password,omitempty"`
-	CongestionControl string              `json:"congestion_control,omitempty"`
-	UDPRelayMode      string              `json:"udp_relay_mode,omitempty"`
-	UDPOverStream     bool                `json:"udp_over_stream,omitempty"`
-	ZeroRTTHandshake  bool                `json:"zero_rtt_handshake,omitempty"`
-	Heartbeat         Duration            `json:"heartbeat,omitempty"`
-	Network           NetworkList         `json:"network,omitempty"`
-	TLS               *OutboundTLSOptions `json:"tls,omitempty"`
+	UUID              string      `json:"uuid,omitempty"`
+	Password          string      `json:"password,omitempty"`
+	CongestionControl string      `json:"congestion_control,omitempty"`
+	UDPRelayMode      string      `json:"udp_relay_mode,omitempty"`
+	UDPOverStream     bool        `json:"udp_over_stream,omitempty"`
+	ZeroRTTHandshake  bool        `json:"zero_rtt_handshake,omitempty"`
+	Heartbeat         Duration    `json:"heartbeat,omitempty"`
+	Network           NetworkList `json:"network,omitempty"`
+	OutboundTLSOptionsContainer
 }
