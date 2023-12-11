0839cd91aa5c1c79f0a1ca0917a4f0918a5e3066
世界 <i@sekai.icu>
2023-12-11 18:36:06 +0800

    Refactor inbound/outbound options struct

--- option/hysteria2.go
+++ option/hysteria2.go
@@ -2,14 +2,14 @@ package option
 
 type Hysteria2InboundOptions struct {
 	ListenOptions
-	UpMbps                int                `json:"up_mbps,omitempty"`
-	DownMbps              int                `json:"down_mbps,omitempty"`
-	Obfs                  *Hysteria2Obfs     `json:"obfs,omitempty"`
-	Users                 []Hysteria2User    `json:"users,omitempty"`
-	IgnoreClientBandwidth bool               `json:"ignore_client_bandwidth,omitempty"`
-	TLS                   *InboundTLSOptions `json:"tls,omitempty"`
-	Masquerade            string             `json:"masquerade,omitempty"`
-	BrutalDebug           bool               `json:"brutal_debug,omitempty"`
+	UpMbps                int             `json:"up_mbps,omitempty"`
+	DownMbps              int             `json:"down_mbps,omitempty"`
+	Obfs                  *Hysteria2Obfs  `json:"obfs,omitempty"`
+	Users                 []Hysteria2User `json:"users,omitempty"`
+	IgnoreClientBandwidth bool            `json:"ignore_client_bandwidth,omitempty"`
+	InboundTLSOptionsContainer
+	Masquerade  string `json:"masquerade,omitempty"`
+	BrutalDebug bool   `json:"brutal_debug,omitempty"`
 }
 
 type Hysteria2Obfs struct {
@@ -25,11 +25,11 @@ type Hysteria2User struct {
 type Hysteria2OutboundOptions struct {
 	DialerOptions
 	ServerOptions
-	UpMbps      int                 `json:"up_mbps,omitempty"`
-	DownMbps    int                 `json:"down_mbps,omitempty"`
-	Obfs        *Hysteria2Obfs      `json:"obfs,omitempty"`
-	Password    string              `json:"password,omitempty"`
-	Network     NetworkList         `json:"network,omitempty"`
-	TLS         *OutboundTLSOptions `json:"tls,omitempty"`
-	BrutalDebug bool                `json:"brutal_debug,omitempty"`
+	UpMbps   int            `json:"up_mbps,omitempty"`
+	DownMbps int            `json:"down_mbps,omitempty"`
+	Obfs     *Hysteria2Obfs `json:"obfs,omitempty"`
+	Password string         `json:"password,omitempty"`
+	Network  NetworkList    `json:"network,omitempty"`
+	OutboundTLSOptionsContainer
+	BrutalDebug bool `json:"brutal_debug,omitempty"`
 }
