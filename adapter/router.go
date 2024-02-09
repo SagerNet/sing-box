3f44f74d95d3962952845728165db6c47a5aa7e5
世界 <i@sekai.icu>
2024-02-09 18:37:25 +0800

    Add support for `client-subnet` DNS options

--- adapter/router.go
+++ adapter/router.go
@@ -86,6 +86,7 @@ type DNSRule interface {
 	Rule
 	DisableCache() bool
 	RewriteTTL() *uint32
+	ClientSubnet() *netip.Addr
 	WithAddressLimit() bool
 	MatchAddressLimit(metadata *InboundContext) bool
 }
