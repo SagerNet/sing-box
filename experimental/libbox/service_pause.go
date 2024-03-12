c2c50d923f4354f23bb55684d630192a68fc9ae4
世界 <i@sekai.icu>
2024-03-12 14:22:31 +0800

    Add test for pause

--- experimental/libbox/service_pause.go
+++ experimental/libbox/service_pause.go
@@ -13,33 +13,28 @@ type servicePauseFields struct {
 func (s *BoxService) Pause() {
 	s.pauseAccess.Lock()
 	defer s.pauseAccess.Unlock()
-
 	if s.pauseTimer != nil {
 		s.pauseTimer.Stop()
 	}
-
 	s.pauseTimer = time.AfterFunc(time.Minute, s.pause)
 }
 
 func (s *BoxService) pause() {
 	s.pauseAccess.Lock()
 	defer s.pauseAccess.Unlock()
-
 	s.pauseManager.DevicePause()
 	_ = s.instance.Router().ResetNetwork()
 	s.pauseTimer = nil
 }
 
 func (s *BoxService) Wake() {
+	_ = s.instance.Router().ResetNetwork()
 	s.pauseAccess.Lock()
 	defer s.pauseAccess.Unlock()
-
 	if s.pauseTimer != nil {
 		s.pauseTimer.Stop()
 		s.pauseTimer = nil
 		return
 	}
-
 	s.pauseManager.DeviceWake()
-	_ = s.instance.Router().ResetNetwork()
 }
