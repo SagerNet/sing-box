7c4c13c3509306c4cf21180fc60aa7232ccb0a69
世界 <i@sekai.icu>
2024-03-26 20:58:47 +0800

    Migrate ntp service to library

--- option/ntp.go
+++ option/ntp.go
@@ -2,9 +2,8 @@ package option
 
 type NTPOptions struct {
 	Enabled       bool     `json:"enabled,omitempty"`
-	Server        string   `json:"server,omitempty"`
-	ServerPort    uint16   `json:"server_port,omitempty"`
 	Interval      Duration `json:"interval,omitempty"`
 	WriteToSystem bool     `json:"write_to_system,omitempty"`
+	ServerOptions
 	DialerOptions
 }
