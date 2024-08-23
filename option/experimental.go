a8067f6eb8c63a4874d6d2dc5c78407323e55da9
世界 <i@sekai.icu>
2024-08-23 13:38:38 +0800

    clash-api: Add PNA support

--- option/experimental.go
+++ option/experimental.go
@@ -17,13 +17,15 @@ type CacheFileOptions struct {
 }
 
 type ClashAPIOptions struct {
-	ExternalController       string   `json:"external_controller,omitempty"`
-	ExternalUI               string   `json:"external_ui,omitempty"`
-	ExternalUIDownloadURL    string   `json:"external_ui_download_url,omitempty"`
-	ExternalUIDownloadDetour string   `json:"external_ui_download_detour,omitempty"`
-	Secret                   string   `json:"secret,omitempty"`
-	DefaultMode              string   `json:"default_mode,omitempty"`
-	ModeList                 []string `json:"-"`
+	ExternalController               string           `json:"external_controller,omitempty"`
+	ExternalUI                       string           `json:"external_ui,omitempty"`
+	ExternalUIDownloadURL            string           `json:"external_ui_download_url,omitempty"`
+	ExternalUIDownloadDetour         string           `json:"external_ui_download_detour,omitempty"`
+	Secret                           string           `json:"secret,omitempty"`
+	DefaultMode                      string           `json:"default_mode,omitempty"`
+	ModeList                         []string         `json:"-"`
+	AccessControlAllowOrigin         Listable[string] `json:"access_control_allow_origin,omitempty"`
+	AccessControlAllowPrivateNetwork bool             `json:"access_control_allow_private_network,omitempty"`
 
 	// Deprecated: migrated to global cache file
 	CacheFile string `json:"cache_file,omitempty"`
