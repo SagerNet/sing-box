d34dda96725025b936ffce5ae6c8f0d66413e84d
世界 <i@sekai.icu>
2024-11-11 16:30:25 +0800

    Add `network_[type/is_expensive/is_constrained]` rule items

--- option/rule.go
+++ option/rule.go
@@ -95,6 +95,9 @@ type RawDefaultRule struct {
 	User                     badoption.Listable[string] `json:"user,omitempty"`
 	UserID                   badoption.Listable[int32]  `json:"user_id,omitempty"`
 	ClashMode                string                     `json:"clash_mode,omitempty"`
+	NetworkType              badoption.Listable[string] `json:"network_type,omitempty"`
+	NetworkIsExpensive       bool                       `json:"network_is_expensive,omitempty"`
+	NetworkIsConstrained     bool                       `json:"network_is_constrained,omitempty"`
 	WIFISSID                 badoption.Listable[string] `json:"wifi_ssid,omitempty"`
 	WIFIBSSID                badoption.Listable[string] `json:"wifi_bssid,omitempty"`
 	RuleSet                  badoption.Listable[string] `json:"rule_set,omitempty"`
