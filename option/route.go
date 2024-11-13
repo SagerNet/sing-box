b2b2843a5b674bbff424ce30b593d61e3117d0a4
世界 <i@sekai.icu>
2024-11-13 19:05:28 +0800

    Refactor multi networks strategy

--- option/route.go
+++ option/route.go
@@ -3,18 +3,20 @@ package option
 import "github.com/sagernet/sing/common/json/badoption"
 
 type RouteOptions struct {
-	GeoIP                  *GeoIPOptions      `json:"geoip,omitempty"`
-	Geosite                *GeositeOptions    `json:"geosite,omitempty"`
-	Rules                  []Rule             `json:"rules,omitempty"`
-	RuleSet                []RuleSet          `json:"rule_set,omitempty"`
-	Final                  string             `json:"final,omitempty"`
-	FindProcess            bool               `json:"find_process,omitempty"`
-	AutoDetectInterface    bool               `json:"auto_detect_interface,omitempty"`
-	OverrideAndroidVPN     bool               `json:"override_android_vpn,omitempty"`
-	DefaultInterface       string             `json:"default_interface,omitempty"`
-	DefaultMark            uint32             `json:"default_mark,omitempty"`
-	DefaultNetworkStrategy NetworkStrategy    `json:"default_network_strategy,omitempty"`
-	DefaultFallbackDelay   badoption.Duration `json:"default_fallback_delay,omitempty"`
+	GeoIP                      *GeoIPOptions                     `json:"geoip,omitempty"`
+	Geosite                    *GeositeOptions                   `json:"geosite,omitempty"`
+	Rules                      []Rule                            `json:"rules,omitempty"`
+	RuleSet                    []RuleSet                         `json:"rule_set,omitempty"`
+	Final                      string                            `json:"final,omitempty"`
+	FindProcess                bool                              `json:"find_process,omitempty"`
+	AutoDetectInterface        bool                              `json:"auto_detect_interface,omitempty"`
+	OverrideAndroidVPN         bool                              `json:"override_android_vpn,omitempty"`
+	DefaultInterface           string                            `json:"default_interface,omitempty"`
+	DefaultMark                FwMark                            `json:"default_mark,omitempty"`
+	DefaultNetworkStrategy     NetworkStrategy                   `json:"default_network_strategy,omitempty"`
+	DefaultNetworkType         badoption.Listable[InterfaceType] `json:"default_network_type,omitempty"`
+	DefaultFallbackNetworkType badoption.Listable[InterfaceType] `json:"default_fallback_network_type,omitempty"`
+	DefaultFallbackDelay       badoption.Duration                `json:"default_fallback_delay,omitempty"`
 }
 
 type GeoIPOptions struct {
