de55ff0cabcd82a6ce23848d1e78d091d4f415ab
世界 <i@sekai.icu>
2024-11-18 18:55:34 +0800

    Migrate bad options to library

--- option/wireguard.go
+++ option/wireguard.go
@@ -1,15 +1,19 @@
 package option
 
-import "net/netip"
+import (
+	"net/netip"
+
+	"github.com/sagernet/sing/common/json/badoption"
+)
 
 type WireGuardOutboundOptions struct {
 	DialerOptions
-	SystemInterface bool                   `json:"system_interface,omitempty"`
-	GSO             bool                   `json:"gso,omitempty"`
-	InterfaceName   string                 `json:"interface_name,omitempty"`
-	LocalAddress    Listable[netip.Prefix] `json:"local_address"`
-	PrivateKey      string                 `json:"private_key"`
-	Peers           []WireGuardPeer        `json:"peers,omitempty"`
+	SystemInterface bool                             `json:"system_interface,omitempty"`
+	GSO             bool                             `json:"gso,omitempty"`
+	InterfaceName   string                           `json:"interface_name,omitempty"`
+	LocalAddress    badoption.Listable[netip.Prefix] `json:"local_address"`
+	PrivateKey      string                           `json:"private_key"`
+	Peers           []WireGuardPeer                  `json:"peers,omitempty"`
 	ServerOptions
 	PeerPublicKey string      `json:"peer_public_key"`
 	PreSharedKey  string      `json:"pre_shared_key,omitempty"`
@@ -21,8 +25,8 @@ type WireGuardOutboundOptions struct {
 
 type WireGuardPeer struct {
 	ServerOptions
-	PublicKey    string           `json:"public_key,omitempty"`
-	PreSharedKey string           `json:"pre_shared_key,omitempty"`
-	AllowedIPs   Listable[string] `json:"allowed_ips,omitempty"`
-	Reserved     []uint8          `json:"reserved,omitempty"`
+	PublicKey    string                     `json:"public_key,omitempty"`
+	PreSharedKey string                     `json:"pre_shared_key,omitempty"`
+	AllowedIPs   badoption.Listable[string] `json:"allowed_ips,omitempty"`
+	Reserved     []uint8                    `json:"reserved,omitempty"`
 }
