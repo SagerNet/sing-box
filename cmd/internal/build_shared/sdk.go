3923b57abfa6c024c4d2691ed5126a8ffde38044
世界 <i@sekai.icu>
2025-01-27 10:33:16 +0800

    release: Update NDK to r28-beta3

--- cmd/internal/build_shared/sdk.go
+++ cmd/internal/build_shared/sdk.go
@@ -48,7 +48,7 @@ func FindSDK() {
 }
 
 func findNDK() bool {
-	const fixedVersion = "28.0.12674087"
+	const fixedVersion = "28.0.12916984"
 	const versionFile = "source.properties"
 	if fixedPath := filepath.Join(androidSDKPath, "ndk", fixedVersion); rw.IsFile(filepath.Join(fixedPath, versionFile)) {
 		androidNDKPath = fixedPath
