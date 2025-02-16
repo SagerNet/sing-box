5f1b0642341873a1f0b7c61349ced478906189ce
世界 <i@sekai.icu>
2025-02-16 11:49:24 +0800

    Add back port hopping to hysteria 1

--- option/hysteria.go
+++ option/hysteria.go
@@ -1,6 +1,9 @@
 package option
 
-import "github.com/sagernet/sing/common/byteformats"
+import (
+	"github.com/sagernet/sing/common/byteformats"
+	"github.com/sagernet/sing/common/json/badoption"
+)
 
 type HysteriaInboundOptions struct {
 	ListenOptions
@@ -26,6 +29,8 @@ type HysteriaUser struct {
 type HysteriaOutboundOptions struct {
 	DialerOptions
 	ServerOptions
+	ServerPorts         badoption.Listable[string]      `json:"server_ports,omitempty"`
+	HopInterval         badoption.Duration              `json:"hop_interval,omitempty"`
 	Up                  *byteformats.NetworkBytesCompat `json:"up,omitempty"`
 	UpMbps              int                             `json:"up_mbps,omitempty"`
 	Down                *byteformats.NetworkBytesCompat `json:"down,omitempty"`
