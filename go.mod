c52a7f7387fa7b032315dd07601c978d16ff45bc
WendelHime <6754291+WendelHime@users.noreply.github.com>
2025-03-03 11:46:49 -0300

    feat: adding outline smart dialer outbound

--- go.mod
+++ go.mod
@@ -1,8 +1,12 @@
 module github.com/sagernet/sing-box
 
-go 1.20
+go 1.22
+
+toolchain go1.22.9
 
 require (
+	github.com/Jigsaw-Code/outline-sdk v0.0.18
+	github.com/Jigsaw-Code/outline-sdk/x v0.0.1
 	github.com/caddyserver/certmagic v0.20.0
 	github.com/cloudflare/circl v1.3.7
 	github.com/cretz/bine v0.2.0
@@ -50,7 +54,8 @@ require (
 	golang.org/x/sys v0.30.0
 	golang.zx2c4.com/wireguard/wgctrl v0.0.0-20230429144221-925a1e7659e6
 	google.golang.org/grpc v1.63.2
-	google.golang.org/protobuf v1.33.0
+	google.golang.org/protobuf v1.35.2
+	gopkg.in/yaml.v3 v3.0.1
 	howett.net/plist v1.0.1
 )
 
@@ -64,41 +69,40 @@ require (
 	github.com/dgrijalva/jwt-go/v4 v4.0.0-preview1 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
-	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/google/btree v1.1.3 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
-	github.com/google/pprof v0.0.0-20231101202521-4ca4178f5c7a // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/hashicorp/yamux v0.1.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/josharian/native v1.1.0 // indirect
-	github.com/klauspost/compress v1.17.4 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
+	github.com/josharian/native v1.1.1-0.20230202152459-5c7d0dd6ab86 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
 	github.com/libdns/libdns v0.2.2 // indirect
 	github.com/mdlayher/netlink v1.7.2 // indirect
-	github.com/mdlayher/socket v0.4.1 // indirect
+	github.com/mdlayher/socket v0.5.1 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
-	github.com/onsi/ginkgo/v2 v2.9.7 // indirect
-	github.com/pierrec/lz4/v4 v4.1.14 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/quic-go/qpack v0.4.0 // indirect
+	github.com/pierrec/lz4/v4 v4.1.21 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/quic-go/qpack v0.5.1 // indirect
 	github.com/quic-go/qtls-go1-20 v0.4.1 // indirect
 	github.com/sagernet/netlink v0.0.0-20240612041022-b9a21c07ac6a // indirect
 	github.com/sagernet/nftables v0.3.0-beta.4 // indirect
+	github.com/shadowsocks/go-shadowsocks2 v0.1.5 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/u-root/uio v0.0.0-20230220225925-ffce2a382923 // indirect
+	github.com/u-root/uio v0.0.0-20240118234441-a3c409a6018e // indirect
 	github.com/vishvananda/netns v0.0.4 // indirect
-	github.com/zeebo/blake3 v0.2.3 // indirect
+	github.com/zeebo/blake3 v0.2.4 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	golang.org/x/time v0.7.0 // indirect
 	golang.org/x/tools v0.24.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
 	golang.zx2c4.com/wintun v0.0.0-20230126152724-0fa3db229ce2 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240227224415-6ceb2ff114de // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241202173237-19429a94021a // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 	lukechampine.com/blake3 v1.3.0 // indirect
 )
---
+++ protocol/outline/outbound.go
@@ -0,0 +1,116 @@
+// Package outline implements the smart dialer outbound using the outline-sdk package.
+// You can find more details here: https://github.com/Jigsaw-Code/outline-sdk/tree/v0.0.18/x/smart
+package outline
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"time"
+
+	otransport "github.com/Jigsaw-Code/outline-sdk/transport"
+	"github.com/Jigsaw-Code/outline-sdk/x/smart"
+	"github.com/sagernet/sing-box/adapter"
+	"github.com/sagernet/sing-box/adapter/outbound"
+	"github.com/sagernet/sing-box/common/dialer"
+	"github.com/sagernet/sing-box/log"
+	"github.com/sagernet/sing-box/option"
+	"github.com/sagernet/sing/common/logger"
+	"github.com/sagernet/sing/common/metadata"
+	"github.com/sagernet/sing/common/network"
+	"gopkg.in/yaml.v3"
+)
+
+// OutlineOutbound implements the smart dialer outbound
+type OutlineOutbound struct {
+	outbound.Adapter
+	logger logger.ContextLogger
+	dialer otransport.StreamDialer
+}
+
+// NewOutlineOutbound creates a proxyless outbond that uses the proxyless transport
+// for dialing
+func NewOutlineOutbound(ctx context.Context, router adapter.Router, logger log.ContextLogger, tag string, options option.OutlineOutboundOptions) (adapter.Outbound, error) {
+	logger.DebugContext(ctx, "creating outline smart dialer outbound")
+	outboundDialer, err := dialer.New(ctx, options.DialerOptions, true)
+	if err != nil {
+		return nil, err
+	}
+
+	if options.TestTimeout == nil {
+		timeout := 10 * time.Second
+		options.TestTimeout = &timeout
+	}
+
+	outboundStreamDialer := &outboundStreamDialer{
+		dialer: outboundDialer,
+		logger: logger,
+	}
+
+	strategyFinder := &smart.StrategyFinder{
+		TestTimeout: *options.TestTimeout,
+		// TODO: define log writer
+		LogWriter:    os.Stdout,
+		StreamDialer: outboundStreamDialer,
+		PacketDialer: outboundStreamDialer,
+	}
+
+	yamlOptions, err := yaml.Marshal(options)
+	if err != nil {
+		return nil, err
+	}
+
+	dialer, err := strategyFinder.NewDialer(ctx, options.Domains, yamlOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	return &OutlineOutbound{
+		Adapter: outbound.NewAdapterWithDialerOptions("outline", tag, []string{network.NetworkTCP}, options.DialerOptions),
+		logger:  logger,
+		dialer:  dialer,
+	}, nil
+}
+
+// DialContext extracts the metadata domain, add the destination to the context
+// and use the proxyless dialer for sending the request
+func (o *OutlineOutbound) DialContext(ctx context.Context, network string, destination metadata.Socksaddr) (net.Conn, error) {
+	ctx, metadata := adapter.ExtendContext(ctx)
+	metadata.Outbound = o.Tag()
+	metadata.Destination = destination
+
+	return o.dialer.DialStream(ctx, fmt.Sprintf("%s:%d", metadata.Domain, destination.Port))
+}
+
+// ListenPacket isn't implemented
+func (o *OutlineOutbound) ListenPacket(ctx context.Context, destination metadata.Socksaddr) (net.PacketConn, error) {
+	return nil, os.ErrInvalid
+}
+
+// wrapper around sing-box's network.Dialer to implement streamDialer interface to pass to a
+// stream dialer as innerSD
+type outboundStreamDialer struct {
+	dialer network.Dialer
+	logger log.ContextLogger
+}
+
+func (s *outboundStreamDialer) DialStream(ctx context.Context, addr string) (otransport.StreamConn, error) {
+	destination := metadata.ParseSocksaddr(addr)
+	conn, err := s.dialer.DialContext(ctx, network.NetworkTCP, destination)
+	if err != nil {
+		s.logger.ErrorContext(ctx, "error dialing %s: %v", addr, err)
+		return nil, err
+	}
+	return conn.(*net.TCPConn), nil
+}
+
+func (s *outboundStreamDialer) DialPacket(ctx context.Context, addr string) (net.Conn, error) {
+	destination := metadata.ParseSocksaddr(addr)
+	conn, err := s.dialer.ListenPacket(ctx, destination)
+	if err != nil {
+		s.logger.ErrorContext(ctx, "error listening packet %s: %v", addr, err)
+		return nil, err
+	}
+	return conn.(*net.UDPConn), nil
+}
