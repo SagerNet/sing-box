f8405ab9da752dde0f36dcbfdd34881231ab6fa1
世界 <i@sekai.icu>
2025-03-15 18:09:49 +0800

    fix format

--- route/rule/rule_action.go
+++ route/rule/rule_action.go
@@ -13,7 +13,7 @@ import (
 	"github.com/sagernet/sing-box/common/sniff"
 	C "github.com/sagernet/sing-box/constant"
 	"github.com/sagernet/sing-box/option"
-	tun "github.com/sagernet/sing-tun"
+	"github.com/sagernet/sing-tun"
 	"github.com/sagernet/sing/common"
 	E "github.com/sagernet/sing/common/exceptions"
 	F "github.com/sagernet/sing/common/format"
