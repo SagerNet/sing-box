c6387b8b8cbbaa75b90f950f7d7e9b06afdaed3e
世界 <i@sekai.icu>
2025-03-15 15:58:12 +0800

    option: Fix marshal legacy DNS options

--- dns/transport/udp.go
+++ dns/transport/udp.go
@@ -42,7 +42,7 @@ func NewUDP(ctx context.Context, logger log.ContextLogger, tag string, options o
 	if err != nil {
 		return nil, err
 	}
-	serverAddr := options.ServerOptions.Build()
+	serverAddr := options.DNSServerAddressOptions.Build()
 	if serverAddr.Port == 0 {
 		serverAddr.Port = 53
 	}
