9a83c088b1a83387fd08487932f8772fe4542cae
世界 <i@sekai.icu>
2025-03-20 20:48:23 +0800

    Explicitly reject detour to empty direct outbounds

--- dns/router.go
+++ dns/router.go
@@ -456,6 +456,6 @@ func (r *Router) LookupReverseMapping(ip netip.Addr) (string, bool) {
 func (r *Router) ResetNetwork() {
 	r.ClearCache()
 	for _, transport := range r.transport.Transports() {
-		transport.Reset()
+		transport.Close()
 	}
 }
