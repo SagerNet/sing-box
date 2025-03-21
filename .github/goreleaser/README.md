fd4892caa62e2d0961d81b78c1119fb9ba728240
世界 <i@sekai.icu>
2025-03-21 20:29:40 +0800

    Test mitm

---
+++ .github/goreleaser/README.md
@@ -0,0 +1,3 @@
+# goreleaser
+
+I'm sorry for this, but I can't afford to renew for now because the subscription is too expensive for an open source project.
---
+++ release/local/install_minimal.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+if [ -d /usr/local/go ]; then
+  export PATH="$PATH:/usr/local/go/bin"
+fi
+
+DIR=$(dirname "$0")
+PROJECT=$DIR/../..
+
+pushd $PROJECT
+go install -v -trimpath -ldflags "-s -w -buildid=" ./cmd/sing-box
+popd
+
+sudo cp $(go env GOPATH)/bin/sing-box /usr/local/bin/
+sudo mkdir -p /usr/local/etc/sing-box
+sudo cp $PROJECT/release/config/config.json /usr/local/etc/sing-box/config.json
+sudo cp $DIR/sing-box.service /etc/systemd/system
+sudo systemctl daemon-reload
