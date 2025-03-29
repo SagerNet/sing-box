c4889e76ff030f4f16b160506c0d4135b206f5c4
世界 <i@sekai.icu>
2025-03-29 17:24:34 +0800

    Add service component type

--- common/tls/acme_stub.go
+++ common/tls/acme_stub.go
@@ -11,6 +11,6 @@ import (
 	E "github.com/sagernet/sing/common/exceptions"
 )
 
-func startACME(ctx context.Context, options option.InboundACMEOptions) (*tls.Config, adapter.Service, error) {
+func startACME(ctx context.Context, options option.InboundACMEOptions) (*tls.Config, adapter.SimpleLifecycle, error) {
 	return nil, nil, E.New(`ACME is not included in this build, rebuild with -tags with_acme`)
 }
