a15b5a2463d9e52945ff71908c2daa97e84a7011
世界 <i@sekai.icu>
2025-04-06 19:51:24 +0800

    Fix `no_drop` not work

--- route/rule/rule_action.go
+++ route/rule/rule_action.go
@@ -279,6 +279,9 @@ func (r *RuleActionReject) Error(ctx context.Context) error {
 	default:
 		panic(F.ToString("unknown reject method: ", r.Method))
 	}
+	if r.NoDrop {
+		return returnErr
+	}
 	r.dropAccess.Lock()
 	defer r.dropAccess.Unlock()
 	timeNow := time.Now()
