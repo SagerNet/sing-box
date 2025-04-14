d6f9414c2d7dbafdf58b53924b986cd3427631cd
世界 <i@sekai.icu>
2025-04-14 22:21:11 +0800

    release: Add openwrt packages

---
+++ docs/installation/tools/install.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+download_beta=false
+download_version=""
+
+for arg in "$@"; do
+	if [[ "$arg" == "--beta" ]]; then
+		download_beta=true
+	elif [[ "$arg" == "--version" ]]; then
+		download_version=true
+	elif [[ "$download_version" == 'true' ]]; then
+		download_version="$arg"
+	else
+		echo "Unknown argument: $arg"
+		echo "Usage: $0 [--beta] [--version <version>]"
+		exit 1
+	fi
+done
+
+if [[ $(command -v dpkg) ]]; then
+	os="linux"
+	arch=$(dpkg --print-architecture)
+	package_suffix=".deb"
+	package_install="dpkg -i"
+elif [[ $(command -v dnf) ]]; then
+	os="linux"
+	arch=$(uname -m)
+	package_suffix=".rpm"
+	package_install="dnf install -y"
+elif [[ $(command -v rpm) ]]; then
+	os="linux"
+	arch=$(uname -m)
+	package_suffix=".rpm"
+	package_install="rpm -i"
+elif [[ $(command -v pacman) ]]; then
+	os="linux"
+	arch=$(uname -m)
+	package_suffix=".pkg.tar.zst"
+	package_install="pacman -U --noconfirm"
+elif [[ $(command -v opkg) ]]; then
+	os="openwrt"
+	source /etc/os-release
+	arch="$OPENWRT_ARCH"
+	package_suffix=".ipk"
+	package_install="opkg update && opkg install -y"
+else
+	echo "Missing supported package manager."
+	exit 1
+fi
+
+if [[ -z "$download_version" ]]; then
+
+	header=""
+	if [ -n "$GITHUB_TOKEN" ]; then
+		header="-H \"Authorization: token $GITHUB_TOKEN\""
+	fi
+
+	if [[ "$download_beta" != 'true' ]]; then
+		download_version=$(curl -s --fail-with-body "$header" https://api.github.com/repos/SagerNet/sing-box/releases/latest |
+			grep tag_name |
+			cut -d ":" -f2 |
+			sed 's/\"//g;s/\,//g;s/\ //g;s/v//')
+	else
+		download_version=$(curl -s --fail-with-body "$header" https://api.github.com/repos/SagerNet/sing-box/releases |
+			grep tag_name |
+			head -n 1 |
+			cut -d ":" -f2 |
+			sed 's/\"//g;s/\,//g;s/\ //g;s/v//')
+	fi
+
+fi
+
+package_name="sing-box_${download_version}_${os}_${arch}${package_suffix}"
+package_url="https://github.com/SagerNet/sing-box/releases/download/v${download_version}/${package_name}"
+
+echo "Downloading $package_url"
+curl --fail-with-body -Lo "$package_name" "$package_url"
+
+if [[ $(command -v sudo) ]]; then
+	package_install="sudo $package_install"
+fi
+
+echo "$package_install $package_name"
+$package_install "$package_name"
+rm "$package_name"
