0f8bd01aadaf6a54528698cba5abadd12ebeafad
安容 <HystericalDragons@proton.me>
2025-04-26 14:03:51 +0800

    Report invalid DNS address early

--- dns/transport/tls.go
+++ dns/transport/tls.go
@@ -54,7 +54,9 @@ func NewTLS(ctx context.Context, logger log.ContextLogger, tag string, options o
 		return nil, err
 	}
 	serverAddr := options.DNSServerAddressOptions.Build()
-	if serverAddr.Port == 0 {
+	if !serverAddr.Addr.IsValid() {
+		return nil, E.New("invalid server address: ", serverAddr)
+	} else if serverAddr.Port == 0 {
 		serverAddr.Port = 853
 	}
 	return &TLSTransport{
