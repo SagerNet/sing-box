3670371f4ba802311d4f3a39367b815a4a5ee422
世界 <i@sekai.icu>
2025-05-03 11:35:53 +0800

    Update utls to v1.7.2

--- common/tls/utls_stub.go
+++ common/tls/utls_stub.go
@@ -5,6 +5,7 @@ package tls
 import (
 	"context"
 
+	"github.com/sagernet/sing-box/log"
 	"github.com/sagernet/sing-box/option"
 	E "github.com/sagernet/sing/common/exceptions"
 )
@@ -14,5 +15,9 @@ func NewUTLSClient(ctx context.Context, serverAddress string, options option.Out
 }
 
 func NewRealityClient(ctx context.Context, serverAddress string, options option.OutboundTLSOptions) (Config, error) {
-	return nil, E.New(`uTLS, which is required by reality client is not included in this build, rebuild with -tags with_utls`)
+	return nil, E.New(`uTLS, which is required by reality is not included in this build, rebuild with -tags with_utls`)
+}
+
+func NewRealityServer(ctx context.Context, logger log.Logger, options option.InboundTLSOptions) (ServerConfig, error) {
+	return nil, E.New(`uTLS, which is required by reality is not included in this build, rebuild with -tags with_utls`)
 }
