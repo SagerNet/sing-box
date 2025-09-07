64c723f3d34f640ca0a7b56f9c4459e1781fd9b8
世界 <i@sekai.icu>
2025-09-07 21:03:32 +0800

    Add support for kTLS
    Reference: https://gitlab.com/go-extension/tls

--- protocol/tailscale/dns_transport.go
+++ protocol/tailscale/dns_transport.go
@@ -166,7 +166,7 @@ func (t *DNSTransport) createResolver(directDialer func() N.Dialer, resolver *dn
 			if serverAddr.Port == 0 {
 				serverAddr.Port = 443
 			}
-			tlsConfig := common.Must1(tls.NewClient(t.ctx, serverAddr.AddrString(), option.OutboundTLSOptions{
+			tlsConfig := common.Must1(tls.NewClient(t.ctx, t.logger, serverAddr.AddrString(), option.OutboundTLSOptions{
 				ALPN: []string{http2.NextProtoTLS, "http/1.1"},
 			}))
 			return transport.NewHTTPSRaw(t.TransportAdapter, t.logger, myDialer, serverURL, http.Header{}, serverAddr, tlsConfig), nil
