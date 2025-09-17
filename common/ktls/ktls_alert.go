a3035a5a663be8890310dabfbfc377ee0f0e6d23
世界 <i@sekai.icu>
2025-09-17 17:49:58 +0800

    Do not use linkname by default to simplify debugging

--- common/ktls/ktls_alert.go
+++ common/ktls/ktls_alert.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build linux && go1.25 && !without_badtls
+//go:build linux && go1.25 && badlinkname
 
 package ktls
 
