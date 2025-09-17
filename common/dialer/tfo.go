ab0869c97293cab701ef2dff290568864ce7064e
世界 <i@sekai.icu>
2025-09-17 18:45:20 +0800

    Update tfo-go to latest

--- common/dialer/tfo.go
+++ common/dialer/tfo.go
@@ -14,7 +14,7 @@ import (
 	M "github.com/sagernet/sing/common/metadata"
 	N "github.com/sagernet/sing/common/network"
 
-	"github.com/metacubex/tfo-go"
+	"github.com/database64128/tfo-go/v2"
 )
 
 type slowOpenConn struct {
