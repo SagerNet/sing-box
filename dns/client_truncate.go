6aa206ba7e0ee39426c5a79b74aa79372a1676e4
Mahdi <80265960+Mahdi-zarei@users.noreply.github.com>
2025-10-03 14:07:45 -0700

    Fix dns truncate

--- dns/client_truncate.go
+++ dns/client_truncate.go
@@ -15,8 +15,7 @@ func TruncateDNSMessage(request *dns.Msg, response *dns.Msg, headroom int) (*buf
 	}
 	responseLen := response.Len()
 	if responseLen > maxLen {
-		copyResponse := *response
-		response = &copyResponse
+		response = response.Copy()
 		response.Truncate(maxLen)
 	}
 	buffer := buf.NewSize(headroom*2 + 1 + responseLen)
