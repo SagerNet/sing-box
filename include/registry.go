f096bb81d5ab4788c20b7d022d79c855bb918668
b00tkitism <alom4hdi@gmail.com>
2025-10-26 14:12:32 +0330

    fix: shadowsocks deprecation message

--- include/registry.go
+++ include/registry.go
@@ -142,8 +142,8 @@ func ServiceRegistry() *service.Registry {
 }
 
 func registerStubForRemovedInbounds(registry *inbound.Registry) {
-	inbound.Register[option.ShadowsocksInboundOptions](registry, C.TypeShadowsocksR, func(ctx context.Context, router adapter.Router, logger log.ContextLogger, tag string, options option.ShadowsocksInboundOptions) (adapter.Inbound, error) {
-		return nil, E.New("ShadowsocksR is deprecated and removed in sing-box 1.6.0")
+	inbound.Register[option.ShadowsocksInboundOptions](registry, C.TypeShadowsocks, func(ctx context.Context, router adapter.Router, logger log.ContextLogger, tag string, options option.ShadowsocksInboundOptions) (adapter.Inbound, error) {
+		return nil, E.New("Shadowsocks is deprecated and removed in sing-box 1.6.0")
 	})
 }
 
