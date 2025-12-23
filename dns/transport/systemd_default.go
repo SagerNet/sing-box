99aa71795d66e21ce6e681df893e6c9da30448e4
Nova <mahdi.zrei@gmail.com>
2025-12-23 06:03:56 +0330

    improve systemd dns handler

--- dns/transport/systemd_default.go
+++ dns/transport/systemd_default.go
@@ -2,21 +2,27 @@ package transport
 
 import (
 	"context"
+	mDNS "github.com/miekg/dns"
 	"github.com/sagernet/sing-box/adapter"
 	C "github.com/sagernet/sing-box/constant"
 	"github.com/sagernet/sing-box/dns"
 	"github.com/sagernet/sing-box/log"
 	"github.com/sagernet/sing-box/option"
+	tun "github.com/sagernet/sing-tun"
 	"github.com/sagernet/sing/common/control"
 	E "github.com/sagernet/sing/common/exceptions"
 	"github.com/sagernet/sing/common/metadata"
 	"github.com/sagernet/sing/service"
 	"os/exec"
 	"strings"
+	"sync"
 )
 
 type SystemdDefault struct {
 	*UDPTransport
+	unspecified   bool
+	ifcMonitor    tun.DefaultInterfaceMonitor
+	manualCheckMu sync.Mutex
 }
 
 func RegisterUnderlying(registry *dns.TransportRegistry) {
@@ -29,25 +35,44 @@ func NewSystemdDefault(ctx context.Context, logger log.ContextLogger, tag string
 		return nil, err
 	}
 	transport := NewUDPRaw(logger, dns.NewTransportAdapterWithRemoteOptions(C.DNSTypeUDP, tag, options), transportDialer, metadata.ParseSocksaddr("0.0.0.0:0"))
-	s := &SystemdDefault{UDPTransport: transport}
+	s := &SystemdDefault{UDPTransport: transport, unspecified: true}
 	nm := service.FromContext[adapter.NetworkManager](ctx)
 	nm.InterfaceMonitor().RegisterCallback(s.handleInterfaceUpdate)
 	s.handleInterfaceUpdate(nm.InterfaceMonitor().DefaultInterface(), 0)
+	s.ifcMonitor = nm.InterfaceMonitor()
 	return s, nil
+}
+
+func (s *SystemdDefault) Exchange(ctx context.Context, message *mDNS.Msg) (*mDNS.Msg, error) {
+	if !s.unspecified {
+		return s.UDPTransport.Exchange(ctx, message)
+	}
+	s.manualCheckMu.Lock()
+	if !s.unspecified {
+		s.manualCheckMu.Unlock()
+		return s.UDPTransport.Exchange(ctx, message)
+	}
+	defaultIfc := s.ifcMonitor.DefaultInterface()
+	if defaultIfc == nil {
+		s.manualCheckMu.Unlock()
+		return nil, E.New("No default interface")
+	}
+	s.handleInterfaceUpdate(defaultIfc, 0)
+	s.manualCheckMu.Unlock()
+	return s.UDPTransport.Exchange(ctx, message)
 }
 
 func (s *SystemdDefault) handleInterfaceUpdate(defaultInterface *control.Interface, flags int) {
 	failedFunc := func() {
-		s.access.Lock()
-		if s.conn != nil {
-			s.conn.Close(E.New("interface changed"))
-		}
+		s.connector.access.Lock()
+		s.connector.Reset()
 		s.serverAddr = metadata.ParseSocksaddr("0.0.0.0:0")
-		s.access.Unlock()
+		s.unspecified = true
+		s.connector.access.Unlock()
 	}
 
 	if defaultInterface == nil {
-		s.logger.Error("No default interface")
+		s.Logger.Error("No default interface")
 		failedFunc()
 		return
 	}
@@ -55,23 +80,22 @@ func (s *SystemdDefault) handleInterfaceUpdate(defaultInterface *control.Interfa
 	args := []string{"-i", defaultInterface.Name, "dns"}
 	res, err := exec.Command(cmd, args...).Output()
 	if err != nil {
-		s.logger.Error("Could not call resolvectl ", err)
+		s.Logger.Error("Could not call resolvectl ", err)
 		failedFunc()
 		return
 	}
 	server, err := s.parseResolvectlOutput(string(res))
 	if err != nil {
-		s.logger.Error("failed to parse resolvectl output ", err)
+		s.Logger.Error("failed to parse resolvectl output ", err)
 		failedFunc()
 		return
 	}
-	s.access.Lock()
-	if s.conn != nil {
-		s.conn.Close(E.New("interface changed"))
-	}
+	s.connector.access.Lock()
+	s.connector.Reset()
 	s.serverAddr = server
-	s.logger.Info("underlying dns set to ", server)
-	s.access.Unlock()
+	s.unspecified = false
+	s.Logger.Info("underlying dns set to ", server)
+	s.connector.access.Unlock()
 }
 
 func (s *SystemdDefault) parseResolvectlOutput(out string) (metadata.Socksaddr, error) {
@@ -79,5 +103,5 @@ func (s *SystemdDefault) parseResolvectlOutput(out string) (metadata.Socksaddr, 
 	if len(spl) < 4 {
 		return metadata.Socksaddr{}, E.New("failed to parse resolvectl output: ", out)
 	}
-	return metadata.ParseSocksaddr(spl[3] + ":53"), nil
+	return metadata.ParseSocksaddr(strings.TrimSpace(spl[3]) + ":53"), nil
 }
