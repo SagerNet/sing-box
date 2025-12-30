8e3d70d197cd292a0fd372517b8a09bc6a6e0727
世界 <i@sekai.icu>
2025-12-30 22:36:19 +0800

    Fix missing relay support for Tailscale

--- option/tailscale.go
+++ option/tailscale.go
@@ -12,17 +12,19 @@ import (
 
 type TailscaleEndpointOptions struct {
 	DialerOptions
-	StateDirectory         string           `json:"state_directory,omitempty"`
-	AuthKey                string           `json:"auth_key,omitempty"`
-	ControlURL             string           `json:"control_url,omitempty"`
-	Ephemeral              bool             `json:"ephemeral,omitempty"`
-	Hostname               string           `json:"hostname,omitempty"`
-	AcceptRoutes           bool             `json:"accept_routes,omitempty"`
-	ExitNode               string           `json:"exit_node,omitempty"`
-	ExitNodeAllowLANAccess bool             `json:"exit_node_allow_lan_access,omitempty"`
-	AdvertiseRoutes        []netip.Prefix   `json:"advertise_routes,omitempty"`
-	AdvertiseExitNode      bool             `json:"advertise_exit_node,omitempty"`
-	UDPTimeout             UDPTimeoutCompat `json:"udp_timeout,omitempty"`
+	StateDirectory             string           `json:"state_directory,omitempty"`
+	AuthKey                    string           `json:"auth_key,omitempty"`
+	ControlURL                 string           `json:"control_url,omitempty"`
+	Ephemeral                  bool             `json:"ephemeral,omitempty"`
+	Hostname                   string           `json:"hostname,omitempty"`
+	AcceptRoutes               bool             `json:"accept_routes,omitempty"`
+	ExitNode                   string           `json:"exit_node,omitempty"`
+	ExitNodeAllowLANAccess     bool             `json:"exit_node_allow_lan_access,omitempty"`
+	AdvertiseRoutes            []netip.Prefix   `json:"advertise_routes,omitempty"`
+	AdvertiseExitNode          bool             `json:"advertise_exit_node,omitempty"`
+	RelayServerPort            *uint16          `json:"relay_server_port,omitempty"`
+	RelayServerStaticEndpoints []netip.AddrPort `json:"relay_server_static_endpoints,omitempty"`
+	UDPTimeout                 UDPTimeoutCompat `json:"udp_timeout,omitempty"`
 }
 
 type TailscaleDNSServerOptions struct {
