39592518b06c69464b85bd15b53cf39b5ba91b65
世界 <i@sekai.icu>
2026-03-05 00:15:37 +0800

    Add Android support for MAC and hostname rule items

--- adapter/neighbor.go
+++ adapter/neighbor.go
@@ -5,9 +5,19 @@ import (
 	"net/netip"
 )
 
+type NeighborEntry struct {
+	Address    netip.Addr
+	MACAddress net.HardwareAddr
+	Hostname   string
+}
+
 type NeighborResolver interface {
 	LookupMAC(address netip.Addr) (net.HardwareAddr, bool)
 	LookupHostname(address netip.Addr) (string, bool)
 	Start() error
 	Close() error
+}
+
+type NeighborUpdateListener interface {
+	UpdateNeighborTable(entries []NeighborEntry)
 }
