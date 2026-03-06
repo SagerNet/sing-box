20f60b8c7bc41bdf02e0bbe6cba507f879e4fe90
世界 <i@sekai.icu>
2026-03-06 08:47:37 +0800

    Add macOS support for MAC and hostname rule items

--- experimental/libbox/service.go
+++ experimental/libbox/service.go
@@ -78,6 +78,7 @@ func (w *platformInterfaceWrapper) OpenInterface(options *tun.Options, platformO
 	}
 	options.FileDescriptor = dupFd
 	w.myTunName = options.Name
+	w.iif.RegisterMyInterface(options.Name)
 	return tun.New(*options)
 }
 
@@ -240,11 +241,14 @@ func (w *neighborUpdateListenerWrapper) UpdateNeighborTable(entries NeighborEntr
 	var result []adapter.NeighborEntry
 	for entries.HasNext() {
 		entry := entries.Next()
+		if entry == nil {
+			continue
+		}
 		address, err := netip.ParseAddr(entry.Address)
 		if err != nil {
 			continue
 		}
-		macAddress, err := net.ParseMAC(entry.MACAddress)
+		macAddress, err := net.ParseMAC(entry.MacAddress)
 		if err != nil {
 			continue
 		}
