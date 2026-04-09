d2b10d3b0926303df62290c49a223d5957395c0d
世界 <i@sekai.icu>
2026-04-09 19:34:54 +0800

    Revert "Also enable certificate store by default on Apple platforms"
    This reverts commit 62cb06c02fc569beb7b2ffa3f0a10ef23e136748.

--- box.go
+++ box.go
@@ -170,7 +170,10 @@ func New(options Options) (*Box, error) {
 
 	var internalServices []adapter.LifecycleService
 	certificateOptions := common.PtrValueOrDefault(options.Certificate)
-	if C.IsAndroid || C.IsDarwin || certificateOptions.Store != "" {
+	if C.IsAndroid || certificateOptions.Store != "" && certificateOptions.Store != C.CertificateStoreSystem ||
+		len(certificateOptions.Certificate) > 0 ||
+		len(certificateOptions.CertificatePath) > 0 ||
+		len(certificateOptions.CertificateDirectoryPath) > 0 {
 		certificateStore, err := certificate.NewStore(ctx, logFactory.NewLogger("certificate"), certificateOptions)
 		if err != nil {
 			return nil, err
