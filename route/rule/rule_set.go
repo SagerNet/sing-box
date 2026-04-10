48ced3501c753cba5a6127d17d8bdbe142712942
世界 <i@sekai.icu>
2026-04-10 11:54:47 +0800

    Add `package_name_regex` route, DNS and headless rule item

--- route/rule/rule_set.go
+++ route/rule/rule_set.go
@@ -60,7 +60,7 @@ func HasHeadlessRule(rules []option.HeadlessRule, cond func(rule option.DefaultH
 }
 
 func isProcessHeadlessRule(rule option.DefaultHeadlessRule) bool {
-	return len(rule.ProcessName) > 0 || len(rule.ProcessPath) > 0 || len(rule.ProcessPathRegex) > 0 || len(rule.PackageName) > 0
+	return len(rule.ProcessName) > 0 || len(rule.ProcessPath) > 0 || len(rule.ProcessPathRegex) > 0 || len(rule.PackageName) > 0 || len(rule.PackageNameRegex) > 0
 }
 
 func isWIFIHeadlessRule(rule option.DefaultHeadlessRule) bool {
