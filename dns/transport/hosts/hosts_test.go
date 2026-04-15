1458ca57710da620df8026b8906b853f196b7c44
世界 <i@sekai.icu>
2026-04-15 17:58:54 +0800

    Standardize hosts path

--- dns/transport/hosts/hosts_test.go
+++ dns/transport/hosts/hosts_test.go
@@ -1,16 +1,29 @@
-package hosts_test
+package hosts
 
 import (
 	"net/netip"
+	"os"
+	"runtime"
 	"testing"
 
-	"github.com/sagernet/sing-box/dns/transport/hosts"
+	E "github.com/sagernet/sing/common/exceptions"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestHosts(t *testing.T) {
 	t.Parallel()
-	require.Equal(t, []netip.Addr{netip.AddrFrom4([4]byte{127, 0, 0, 1}), netip.IPv6Loopback()}, hosts.NewFile("testdata/hosts").Lookup("localhost"))
-	require.NotEmpty(t, hosts.NewFile(hosts.DefaultPath).Lookup("localhost"))
+	require.Equal(t, []netip.Addr{netip.AddrFrom4([4]byte{127, 0, 0, 1}), netip.IPv6Loopback()}, NewFile("testdata/hosts").Lookup("localhost"))
+	if runtime.GOOS != "windows" {
+		defaultPathResolved, err := defaultPath()
+		if err != nil {
+			t.Fatal(E.Cause(err, "resolve default hosts path"))
+		}
+		content, readErr := os.ReadFile(defaultPathResolved)
+		require.NoError(t, readErr)
+		hFile := NewFile(defaultPathResolved)
+		if len(hFile.Lookup("localhost")) == 0 {
+			t.Fatal("failed to resolve localhost: ", defaultPathResolved, ": \n", string(content))
+		}
+	}
 }
