34d00562ea36fe681624f62c77e057825e09e4ef
世界 <i@sekai.icu>
2026-04-28 08:18:08 +0800

    Improve oom-killer

--- service/oomkiller/service_darwin.go
+++ service/oomkiller/service_darwin.go
@@ -33,7 +33,6 @@ static void stopMemoryPressureMonitor() {
 import "C"
 
 import (
-	runtimeDebug "runtime/debug"
 	"sync"
 
 	"github.com/sagernet/sing-box/adapter"
@@ -90,7 +89,6 @@ func (s *Service) Close() error {
 
 //export goMemoryPressureCallback
 func goMemoryPressureCallback(status C.ulong) {
-	runtimeDebug.FreeOSMemory()
 	globalAccess.Lock()
 	services := make([]*Service, len(globalServices))
 	copy(services, globalServices)
