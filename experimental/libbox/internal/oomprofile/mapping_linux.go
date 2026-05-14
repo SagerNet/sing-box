f39979cf3f6d462b1c39cc94ccfef8579e3c9400
世界 <i@sekai.icu>
2026-05-14 22:53:22 +0800

    Fix lint errors

--- experimental/libbox/internal/oomprofile/mapping_linux.go
+++ experimental/libbox/internal/oomprofile/mapping_linux.go
@@ -6,7 +6,9 @@ import "os"
 
 func (b *profileBuilder) readMapping() {
 	data, _ := os.ReadFile("/proc/self/maps")
-	stdParseProcSelfMaps(data, b.addMapping)
+	stdParseProcSelfMaps(data, func(lo, hi, offset uint64, file, buildID string) {
+		b.addMappingEntry(lo, hi, offset, file, buildID, false)
+	})
 	if len(b.mem) == 0 {
 		b.addMappingEntry(0, 0, 0, "", "", true)
 	}
