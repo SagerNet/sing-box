76eaa6a7122b0f3559835c65494cd49967ce084c
世界 <i@sekai.icu>
2026-05-21 21:38:29 +0800

    Fix tailscale dns

--- protocol/tailscale/endpoint.go
+++ protocol/tailscale/endpoint.go
@@ -86,6 +86,7 @@ type Endpoint struct {
 	ctx               context.Context
 	router            adapter.Router
 	logger            logger.ContextLogger
+	queryOptions      adapter.DNSQueryOptions
 	dnsRouter         adapter.DNSRouter
 	network           adapter.NetworkManager
 	platformInterface adapter.PlatformInterface
@@ -181,6 +182,7 @@ func NewEndpoint(ctx context.Context, router adapter.Router, logger log.ContextL
 		router:            router,
 		logger:            logger,
 		dnsRouter:         dnsRouter,
+		queryOptions:      dialerQueryOptions,
 		network:           service.FromContext[adapter.NetworkManager](ctx),
 		platformInterface: service.FromContext[adapter.PlatformInterface](ctx),
 		server: &tsnet.Server{
