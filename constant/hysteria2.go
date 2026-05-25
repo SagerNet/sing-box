ab8b803bd145b87a0110bd356d39a84f784ee348
世界 <i@sekai.icu>
2026-05-25 13:11:59 +0800

    hysteria2: Add gecko obfs

--- constant/hysteria2.go
+++ constant/hysteria2.go
@@ -1,6 +1,11 @@
 package constant
 
 const (
+	Hysteria2ObfsTypeSalamander = "salamander"
+	Hysteria2ObfsTypeGecko      = "gecko"
+)
+
+const (
 	Hysterai2MasqueradeTypeFile   = "file"
 	Hysterai2MasqueradeTypeProxy  = "proxy"
 	Hysterai2MasqueradeTypeString = "string"
