29826ddae51de11f5a128ded31ceb6fc0df8984c
世界 <i@sekai.icu>
2026-07-11 15:52:20 +0800

    Fix netns manager

--- common/netns/manager_stub.go
+++ common/netns/manager_stub.go
@@ -3,18 +3,33 @@
 package netns
 
 import (
+	"github.com/sagernet/sing-box/adapter"
+	"github.com/sagernet/sing-box/option"
 	E "github.com/sagernet/sing/common/exceptions"
+	"github.com/sagernet/sing/common/logger"
 )
 
-type holder struct{}
+type Manager struct{}
 
-func (m *Manager) start() error {
-	if len(m.namespaces) > 0 {
-		return E.New("network namespaces are only supported on Linux")
+func NewManager(logger logger.ContextLogger, namespaces []option.NetworkNamespace, holderArgs []string) (*Manager, error) {
+	if len(namespaces) > 0 {
+		return nil, E.New("network namespaces are only supported on Linux")
 	}
+	return &Manager{}, nil
+}
+
+func (m *Manager) Name() string {
+	return "netns"
+}
+
+func (m *Manager) Start(stage adapter.StartStage) error {
 	return nil
 }
 
-func (m *Manager) close() error {
+func (m *Manager) Close() error {
 	return nil
+}
+
+func (m *Manager) ResolvePath(nameOrPath string) string {
+	return nameOrPath
 }
