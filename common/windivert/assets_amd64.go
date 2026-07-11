2ffce9421ffc16771551d8d90033635c4058c028
世界 <i@sekai.icu>
2026-07-11 16:13:22 +0800

    windivert: Verify cached driver against embedded asset before load

--- common/windivert/assets_amd64.go
+++ common/windivert/assets_amd64.go
@@ -7,8 +7,4 @@ import _ "embed"
 //go:embed assets/WinDivert64.sys
 var sysBytes []byte
 
-func assetFiles() []assetFile {
-	return []assetFile{{"WinDivert64.sys", sysBytes}}
-}
-
 func driverSysName() string { return "WinDivert64.sys" }
