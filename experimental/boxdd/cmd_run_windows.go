7b54ea88b22d2f261ea1f23f7ca86b96c5a997fe
世界 <i@sekai.icu>
2026-07-14 18:56:28 +0800

    boxdd: Fix log format

--- experimental/boxdd/cmd_run_windows.go
+++ experimental/boxdd/cmd_run_windows.go
@@ -85,7 +85,7 @@ func (s *windowsService) Execute(arguments []string, requests <-chan svc.ChangeR
 		if request.Cmd == svc.Stop || request.Cmd == svc.Shutdown {
 			break
 		}
-		serviceLogError(E.New("unexpected service command: ", request.Cmd))
+		serviceLogError(E.New("unexpected service command: ", uint32(request.Cmd)))
 	}
 	statuses <- svc.Status{State: svc.StopPending}
 	watchdog := time.AfterFunc(3*time.Second, func() {
