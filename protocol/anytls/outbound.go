020105e5f6d7c2ef996669e5919492573d87e236
世界 <i@sekai.icu>
2026-07-15 20:50:36 +0800

    Fix start lifecycle

--- protocol/anytls/outbound.go
+++ protocol/anytls/outbound.go
@@ -30,9 +30,11 @@ var _ adapter.OutboundWithMultiplex = (*Outbound)(nil)
 
 type Outbound struct {
 	outbound.Adapter
+	ctx            context.Context
 	dialer         tls.Dialer
 	server         M.Socksaddr
 	tlsConfig      tls.Config
+	clientOptions  anytls.ClientConfig
 	clientMetadata string
 	client         *anytls.Client
 	sessionClient  *session.Client
@@ -43,6 +45,7 @@ type Outbound struct {
 func NewOutbound(ctx context.Context, router adapter.Router, logger log.ContextLogger, tag string, options option.AnyTLSOutboundOptions) (adapter.Outbound, error) {
 	outbound := &Outbound{
 		Adapter: outbound.NewAdapterWithDialerOptions(C.TypeAnyTLS, tag, []string{N.NetworkTCP, N.NetworkUDP}, options.DialerOptions),
+		ctx:     ctx,
 		server:  options.ServerOptions.Build(),
 		logger:  logger,
 	}
@@ -74,26 +77,33 @@ func NewOutbound(ctx context.Context, router adapter.Router, logger log.ContextL
 
 	outbound.dialer = tls.NewDialer(outboundDialer, tlsConfig)
 
-	client, err := anytls.NewClient(ctx, anytls.ClientConfig{
+	outbound.clientOptions = anytls.ClientConfig{
 		Password:                 options.Password,
 		IdleSessionCheckInterval: options.IdleSessionCheckInterval.Build(),
 		IdleSessionTimeout:       options.IdleSessionTimeout.Build(),
 		MinIdleSession:           options.MinIdleSession,
 		DialOut:                  outbound.dialOut,
 		Logger:                   logger,
-	})
-	if err != nil {
-		return nil, err
 	}
-	outbound.client = client
 	outbound.clientMetadata = options.ClientMetadata
-	outbound.sessionClient = sessionClientOf(client)
+	return outbound, nil
+}
 
-	outbound.uotClient = &uot.Client{
-		Dialer:  (anytlsDialer)(outbound.createProxy),
+func (h *Outbound) Start(stage adapter.StartStage) error {
+	if stage != adapter.StartStateInitialize {
+		return nil
+	}
+	client, err := anytls.NewClient(h.ctx, h.clientOptions)
+	if err != nil {
+		return err
+	}
+	h.client = client
+	h.sessionClient = sessionClientOf(client)
+	h.uotClient = &uot.Client{
+		Dialer:  (anytlsDialer)(h.createProxy),
 		Version: uot.Version,
 	}
-	return outbound, nil
+	return nil
 }
 
 func (h *Outbound) createProxy(ctx context.Context, destination M.Socksaddr) (net.Conn, error) {
@@ -152,5 +162,5 @@ func (h *Outbound) ListenPacket(ctx context.Context, destination M.Socksaddr) (n
 }
 
 func (h *Outbound) Close() error {
-	return common.Close(h.client)
+	return common.Close(common.PtrOrNil(h.client))
 }
