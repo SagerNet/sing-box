e0a3031fa84ebd368e0cb062a59b6ec35e8c3546
世界 <i@sekai.icu>
2026-07-18 09:40:17 +0800

    Refactor OpenVPN and OpenConnect endpoints

--- test/testdata/openconnect/ocserv/echo_server.py
+++ test/testdata/openconnect/ocserv/echo_server.py
@@ -12,11 +12,21 @@ def echo(connection):
             connection.sendall(data)
 
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind(("0.0.0.0", 18080))
-listener.listen()
+def echo_udp(connection):
+    while True:
+        data, address = connection.recvfrom(65536)
+        connection.sendto(data, address)
+
+
+udp_listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_listener.bind(("0.0.0.0", 18080))
+threading.Thread(target=echo_udp, args=(udp_listener,), daemon=True).start()
+
+tcp_listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcp_listener.bind(("0.0.0.0", 18080))
+tcp_listener.listen()
 print("openconnect echo ready", flush=True)
 while True:
-    accepted, _ = listener.accept()
+    accepted, _ = tcp_listener.accept()
     threading.Thread(target=echo, args=(accepted,), daemon=True).start()
