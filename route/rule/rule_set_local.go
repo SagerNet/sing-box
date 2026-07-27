890544e88b165f84d19d09a068e35161ffd509e3
世界 <i@sekai.icu>
2026-07-27 10:49:29 +0800

    Fix rule-set file descriptor leak

--- route/rule/rule_set_local.go
+++ route/rule/rule_set_local.go
@@ -114,6 +114,7 @@ func (s *LocalRuleSet) reloadFile(path string) error {
 		if err != nil {
 			return err
 		}
+		defer setFile.Close()
 		ruleSet, err = srs.Read(setFile, false)
 		if err != nil {
 			return err
