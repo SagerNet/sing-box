7da573f1873ec9a4b13a31cd04e7c0f940d309de
世界 <i@sekai.icu>
2026-07-31 10:59:25 +0800

    direct: Fix routing loop to exact TUN address on darwin

--- protocol/direct/outbound.go
+++ protocol/direct/outbound.go
@@ -108,7 +108,10 @@ func (h *Outbound) fetchMyAddresses() {
 func (h *Outbound) isMyLoopbackAddress(addresses ...netip.Addr) bool {
 	for _, prefix := range h.myAddresses.Load() {
 		for _, address := range addresses {
-			if prefix.Addr() != address && prefix.Contains(address) {
+			if !C.IsDarwin && prefix.Addr() == address {
+				continue
+			}
+			if prefix.Contains(address) {
 				return true
 			}
 		}
