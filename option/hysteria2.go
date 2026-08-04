10a4ee0d90bf669c0ba9999d9c588d494facb908
世界 <i@sekai.icu>
2026-08-05 00:11:13 +0800

    Add hysteria2 chrome parrot support

--- option/hysteria2.go
+++ option/hysteria2.go
@@ -212,9 +212,10 @@ type Hysteria2OutboundOptions struct {
 	Network        NetworkList                `json:"network,omitempty"`
 	OutboundTLSOptionsContainer
 	QUICOptions
-	BBRProfile  string          `json:"bbr_profile,omitempty" enum:"standard,conservative,aggressive"`
-	BrutalDebug bool            `json:"brutal_debug,omitempty"`
-	Realm       *Hysteria2Realm `json:"realm,omitempty"`
+	BBRProfile          string          `json:"bbr_profile,omitempty" enum:"standard,conservative,aggressive"`
+	BrutalDebug         bool            `json:"brutal_debug,omitempty"`
+	DisableChromeParrot bool            `json:"disable_chrome_parrot,omitempty"`
+	Realm               *Hysteria2Realm `json:"realm,omitempty"`
 }
 
 type HysteriaRealmUser struct {
