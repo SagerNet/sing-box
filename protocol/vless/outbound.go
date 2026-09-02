a57240e37527a1418bf62f0ebbca34b74a1db53b
世界 <i@sekai.icu>
2026-09-02 22:28:27 +0800

    Close idle connections of unreferenced outbounds and DNS servers

--- protocol/vless/outbound.go
+++ protocol/vless/outbound.go
@@ -27,7 +27,11 @@ func RegisterOutbound(registry *outbound.Registry) {
 	outbound.Register[option.VLESSOutboundOptions](registry, C.TypeVLESS, NewOutbound)
 }
 
-var _ adapter.OutboundWithMultiplex = (*Outbound)(nil)
+var (
+	_ adapter.OutboundWithMultiplex   = (*Outbound)(nil)
+	_ adapter.InterfaceUpdateListener = (*Outbound)(nil)
+	_ adapter.IdleConnectionCloser    = (*Outbound)(nil)
+)
 
 type Outbound struct {
 	outbound.Adapter
@@ -139,6 +143,16 @@ func (h *Outbound) InterfaceUpdated(ctx context.Context) {
 	}
 	if h.multiplexDialer != nil {
 		h.multiplexDialer.Reset()
+	}
+}
+
+func (h *Outbound) CloseIdleConnections() {
+	transportCloser, isTransportCloser := h.transport.(adapter.IdleConnectionCloser)
+	if isTransportCloser {
+		transportCloser.CloseIdleConnections()
+	}
+	if h.multiplexDialer != nil {
+		h.multiplexDialer.CloseIdleConnections()
 	}
 }
 
