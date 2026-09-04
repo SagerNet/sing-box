1d82e8e7f6cc6564de05c6c508ba452a26db0ca6
世界 <i@sekai.icu>
2026-09-04 21:32:13 +0800

    Improve process search

--- service/resolved/resolve1.go
+++ service/resolved/resolve1.go
@@ -125,11 +125,11 @@ func (t *resolve1Manager) createMetadata(sender dbus.Sender) adapter.InboundCont
 
 	processPath, err := os.Readlink(F.ToString("/proc/", senderPid, "/exe"))
 	if err == nil {
-		processInfo.ProcessPath = processPath
+		processInfo.ProcessPaths = []string{processPath}
 	} else {
 		processName, readErr := os.ReadFile(F.ToString("/proc/", senderPid, "/comm"))
 		if readErr == nil {
-			processInfo.ProcessPath = strings.TrimSpace(string(processName))
+			processInfo.ProcessPaths = []string{strings.TrimSpace(string(processName))}
 		}
 	}
 
@@ -165,8 +165,8 @@ func (t *resolve1Manager) log(sender dbus.Sender, message ...any) {
 	metadata := t.createMetadata(sender)
 	if metadata.ProcessInfo != nil {
 		var prefix string
-		if metadata.ProcessInfo.ProcessPath != "" {
-			prefix = filepath.Base(metadata.ProcessInfo.ProcessPath)
+		if len(metadata.ProcessInfo.ProcessPaths) > 0 {
+			prefix = filepath.Base(metadata.ProcessInfo.ProcessPaths[0])
 		} else if metadata.ProcessInfo.UserName != "" {
 			prefix = F.ToString("user:", metadata.ProcessInfo.UserName)
 		} else if metadata.ProcessInfo.UserId != 0 {
@@ -183,8 +183,8 @@ func (t *resolve1Manager) logRequest(sender dbus.Sender, message ...any) context
 	metadata := t.createMetadata(sender)
 	if metadata.ProcessInfo != nil {
 		var prefix string
-		if metadata.ProcessInfo.ProcessPath != "" {
-			prefix = filepath.Base(metadata.ProcessInfo.ProcessPath)
+		if len(metadata.ProcessInfo.ProcessPaths) > 0 {
+			prefix = filepath.Base(metadata.ProcessInfo.ProcessPaths[0])
 		} else if metadata.ProcessInfo.UserName != "" {
 			prefix = F.ToString("user:", metadata.ProcessInfo.UserName)
 		} else if metadata.ProcessInfo.UserId != 0 {
