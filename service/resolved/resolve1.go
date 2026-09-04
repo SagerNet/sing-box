c827ec83035545a74a0df521185d43d0a0876d5c
世界 <i@sekai.icu>
2026-09-04 22:55:44 +0800

    Read process name from comm in resolved fallback

--- service/resolved/resolve1.go
+++ service/resolved/resolve1.go
@@ -119,9 +119,9 @@ func (t *resolve1Manager) createMetadata(sender dbus.Sender) adapter.InboundCont
 	if err == nil {
 		processInfo.ProcessPath = processPath
 	} else {
-		processPath, err = os.Readlink(F.ToString("/proc/", senderPid, "/comm"))
-		if err == nil {
-			processInfo.ProcessPath = processPath
+		processName, readErr := os.ReadFile(F.ToString("/proc/", senderPid, "/comm"))
+		if readErr == nil {
+			processInfo.ProcessPath = strings.TrimSpace(string(processName))
 		}
 	}
 
