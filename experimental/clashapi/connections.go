e35f73d8d3f7a24fcdb85247c3c3e104b074b8ff
世界 <i@sekai.icu>
2026-09-04 21:32:13 +0800

    Improve process search

--- experimental/clashapi/connections.go
+++ experimental/clashapi/connections.go
@@ -61,10 +61,10 @@ func (c connectionObject) MarshalJSON() ([]byte, error) {
 	}
 	var processPath string
 	if c.Metadata.ProcessInfo != nil {
-		if c.Metadata.ProcessInfo.ProcessPath != "" {
-			processPath = c.Metadata.ProcessInfo.ProcessPath
-		} else if len(c.Metadata.ProcessInfo.AndroidPackageNames) > 0 {
-			processPath = c.Metadata.ProcessInfo.AndroidPackageNames[0]
+		if len(c.Metadata.ProcessInfo.ProcessPaths) > 0 {
+			processPath = c.Metadata.ProcessInfo.ProcessPaths[0]
+		} else if len(c.Metadata.ProcessInfo.PackageNames) > 0 {
+			processPath = c.Metadata.ProcessInfo.PackageNames[0]
 		}
 		if processPath == "" {
 			if c.Metadata.ProcessInfo.UserId != -1 {
