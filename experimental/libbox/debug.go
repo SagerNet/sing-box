864ce014648d293be1f7549a78e75373960fb5d7
世界 <i@sekai.icu>
2026-09-06 06:53:12 +0800

    Add goroutine dump and hang trigger to libbox

--- experimental/libbox/debug.go
+++ experimental/libbox/debug.go
@@ -1,6 +1,7 @@
 package libbox
 
 import (
+	"runtime"
 	"time"
 	"unsafe"
 )
@@ -9,4 +10,19 @@ func TriggerGoPanic() {
 	time.AfterFunc(200*time.Millisecond, func() {
 		*(*int)(unsafe.Pointer(uintptr(0))) = 0
 	})
+}
+
+func TriggerGoHang(seconds int32) {
+	time.Sleep(time.Duration(seconds) * time.Second)
+}
+
+func GoroutineDump() string {
+	buffer := make([]byte, 64*1024)
+	for {
+		n := runtime.Stack(buffer, true)
+		if n < len(buffer) {
+			return string(buffer[:n])
+		}
+		buffer = make([]byte, 2*len(buffer))
+	}
 }
