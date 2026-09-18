e13b457ae42be1c077e7ac5ba74be2c30e6242b1
世界 <i@sekai.icu>
2026-09-18 23:11:13 +0800

    Fix DNS sniffer matching non-DNS packets

--- common/sniff/dns.go
+++ common/sniff/dns.go
@@ -14,44 +14,90 @@ import (
 	mDNS "github.com/miekg/dns"
 )
 
+const dnsHeaderSize = 12
+
 func StreamDomainNameQuery(readCtx context.Context, metadata *adapter.InboundContext, reader io.Reader) error {
 	var length uint16
 	err := binary.Read(reader, binary.BigEndian, &length)
 	if err != nil {
 		return E.Cause1(ErrNeedMoreData, err)
 	}
-	if length < 12 {
+	if length < dnsHeaderSize {
 		return os.ErrInvalid
 	}
 	buffer := buf.NewSize(int(length))
 	defer buffer.Release()
-	var n int
-	n, err = buffer.ReadFullFrom(reader, buffer.FreeLen())
-	packet := buffer.Bytes()
-	if n > 2 && packet[2]&0x80 != 0 { // QR
-		return os.ErrInvalid
-	}
-	if n > 5 && packet[4] == 0 && packet[5] == 0 { // QDCOUNT
-		return os.ErrInvalid
-	}
-	for i := 6; i < 10; i++ {
-		// ANCOUNT, NSCOUNT
-		if n > i && packet[i] != 0 {
-			return os.ErrInvalid
-		}
-	}
+	_, err = buffer.ReadFullFrom(reader, buffer.FreeLen())
 	if err != nil {
+		headerErr := verifyDNSQueryHeader(buffer.Bytes())
+		if headerErr != nil {
+			return headerErr
+		}
 		return E.Cause1(ErrNeedMoreData, err)
 	}
-	return DomainNameQuery(readCtx, metadata, packet)
+	return DomainNameQuery(readCtx, metadata, buffer.Bytes())
 }
 
 func DomainNameQuery(ctx context.Context, metadata *adapter.InboundContext, packet []byte) error {
-	var msg mDNS.Msg
-	err := msg.Unpack(packet)
-	if err != nil || msg.Response || len(msg.Question) == 0 || len(msg.Answer) > 0 || len(msg.Ns) > 0 {
+	if len(packet) < dnsHeaderSize {
+		return os.ErrInvalid
+	}
+	err := verifyDNSQueryHeader(packet)
+	if err != nil {
 		return err
 	}
+	offset := dnsHeaderSize
+	questionCount := int(binary.BigEndian.Uint16(packet[4:6]))
+	for i := 0; i < questionCount; i++ {
+		_, offset, err = mDNS.UnpackDomainName(packet, offset)
+		if err != nil {
+			return err
+		}
+		if offset+4 > len(packet) {
+			return os.ErrInvalid
+		}
+		offset += 4
+	}
+	additionalCount := int(binary.BigEndian.Uint16(packet[10:12]))
+	for i := 0; i < additionalCount; i++ {
+		_, offset, err = mDNS.UnpackRR(packet, offset)
+		if err != nil {
+			return err
+		}
+	}
+	if offset != len(packet) {
+		return os.ErrInvalid
+	}
 	metadata.Protocol = C.ProtocolDNS
+	return nil
+}
+
+func verifyDNSQueryHeader(header []byte) error {
+	if len(header) > 2 {
+		isResponse := header[2]&0x80 != 0
+		opcode := int(header[2]>>3) & 0x0F
+		authoritative := header[2]&0x04 != 0
+		truncated := header[2]&0x02 != 0
+		if isResponse || opcode != mDNS.OpcodeQuery || authoritative || truncated {
+			return os.ErrInvalid
+		}
+	}
+	if len(header) > 3 {
+		recursionAvailable := header[3]&0x80 != 0
+		reserved := header[3]&0x40 != 0
+		responseCode := int(header[3] & 0x0F)
+		if recursionAvailable || reserved || responseCode != mDNS.RcodeSuccess {
+			return os.ErrInvalid
+		}
+	}
+	if len(header) > 5 && binary.BigEndian.Uint16(header[4:6]) == 0 {
+		return os.ErrInvalid
+	}
+	if len(header) > 7 && binary.BigEndian.Uint16(header[6:8]) != 0 {
+		return os.ErrInvalid
+	}
+	if len(header) > 9 && binary.BigEndian.Uint16(header[8:10]) != 0 {
+		return os.ErrInvalid
+	}
 	return nil
 }
